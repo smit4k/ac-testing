9fd9a1483c2b57c387012288d2bcec88d3d00251
Saber <smit.patil.usa@gmail.com>
2024-12-27 14:58:39 -0500

    Add anti cheating script

---
+++ script.js
@@ -0,0 +1,26 @@
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
+setInterval(() => {
+    const start = performance.now();
+    debugger;
+    const end = performance.now();
+    if (end - start > 100) {
+      alert("Developer tools detected. Cheating is not allowed.");
+      window.location.href = '/cheaterdetected.html';
+    }
+  }, 1000);
+
+  
